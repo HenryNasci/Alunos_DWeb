58057a9670e919c7a4679449c00bad72d1c92891
Henrique Nascimento <aluno24173@ipt.pt>
2024-03-12 10:39:28 +0000

    commit inicial

---
+++ Alunos-Dweb/Alunos-Dweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alunos_Dweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
